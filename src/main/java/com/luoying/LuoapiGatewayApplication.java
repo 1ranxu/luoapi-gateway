8788e77d91a586d1fcf54fb1d4d210ff4aa3faec
1ranxu <263989312@qq.com>
2023-10-16 22:00:40 +0800

    整合nacos和dubbo，远程调用backend项目提供的接口

--- src/main/java/com/luoying/LuoapiGatewayApplication.java
+++ src/main/java/com/luoying/LuoapiGatewayApplication.java
@@ -1,17 +1,39 @@
 package com.luoying;
 
+import com.luoying.provider.DemoService;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
+@EnableDubbo
 public class LuoapiGatewayApplication {
 
+    @DubboReference
+    private DemoService demoService;
+
+
     public static void main(String[] args) {
-        SpringApplication.run(LuoapiGatewayApplication.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(LuoapiGatewayApplication.class, args);
+        LuoapiGatewayApplication application = context.getBean(LuoapiGatewayApplication.class);
+        String result = application.doSayHello("world");
+        String result2 = application.doSayHello2("world");
+        System.out.println("result: " + result);
+        System.out.println("result: " + result2);
     }
+
+    public String doSayHello(String name) {
+        return demoService.sayHello(name);
+    }
+
+    public String doSayHello2(String name) {
+        return demoService.sayHello2(name);
+    }
+
 
 /*    @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
